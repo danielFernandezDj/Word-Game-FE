ef57fc510a23fb25c5fde76d9163de59bd89fa47
Daniel Fernandez <mdanif9728@gmail.com>
2024-10-02 17:10:19 -0700

    fixin the border for small screen at spelling-bee

--- src/components/SpellingBee.js
+++ src/components/SpellingBee.js
@@ -221,7 +221,7 @@ function SpellingBee() {
 
       <br /> <hr /> <br />
 
-      <div className="border-solid rounded-xl border-2 border-indigo-400 border-dotted
+      <div className="md:border-solid md:rounded-xl md:border-2 md:border-indigo-400 md:border-dotted
           p-8 bg-white
         ">
         <div className="flex flex-row justify-between">
@@ -304,7 +304,7 @@ function SpellingBee() {
       </button>
 
       {showInstructions && (
-        <div className="border-solid rounded-xl border-2 border-indigo-400 border-dotted
+        <div className="md:border-solid md:rounded-xl md:border-2 md:border-indigo-400 md:border-dotted
             m-auto max-w-2xl p-8 bg-white
           ">
 

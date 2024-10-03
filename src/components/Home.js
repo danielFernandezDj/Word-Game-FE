e5c4aa78687b152053c14cd7f81b91042faa65d2
Daniel Fernandez <mdanif9728@gmail.com>
2024-10-03 12:05:30 -0700

    adding the new planet

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,9 @@ import SudokuPanel from './homePanels/SudokuPanel';
 
 const Home = () => {
   const [panelComponent, setPanelComponent] = useState(<WordlePanel />);
-
+  // !-----------------------!-----------------------!
+  //! I want to include a new planet that set (Coming Sun!).
+  // !-----------------------!-----------------------!
   return (
     <div className="text-center mt-8">
       <h1 className="text-6xl font-bold">
@@ -17,24 +19,28 @@ const Home = () => {
         Challenge yourself with our exciting word games!
       </p>
       <hr />
-      <div className="flex flex-wrap justify-center">
-        <div className='basis-1/3 m-6 flex flex-col justify-center hover:scale-110 transition-transform duration-300'
+      <div className="flex flex-wrap md:space-x-1 space-x-5 justify-center">
+        <div className='basis-1/3 flex flex-col justify-center hover:scale-110 transition-transform duration-300'
           onClick={() => setPanelComponent(<WordlePanel />)}
         >
           <img src="/images/planets/planet-1.png" alt="Wordle Planet" className="hover:drop-shadow-2xl" />
           <h2 className="text-3xl font-bold text-zinc-900">Wordle</h2>
         </div>
-        <div className='basis-1/4 m-6 flex flex-col justify-center hover:scale-110 transition-transform duration-300'
+        <div className='basis-1/4 flex flex-col justify-center hover:scale-110 transition-transform duration-300'
           onClick={() => setPanelComponent(<SpellingPanel />)}
         >
           <img src="/images/planets/planet-2.png" alt="Spelling-Bee Planet" className="hover:drop-shadow-2xl" />
           <h2 className="text-3xl font-bold text-zinc-900">Spelling</h2>
         </div>
-        <div className='basis-1/5 m-6 flex flex-col justify-center hover:scale-110 transition-transform duration-300'
+        <div className='basis-1/5 flex flex-col justify-center hover:scale-110 transition-transform duration-300'
           onClick={() => setPanelComponent(<SudokuPanel />)}
         >
           <img src="/images/planets/planet-3.png" alt="Sudoku Planet" className="hover:drop-shadow-2xl" />
           <h2 className="text-3xl font-bold text-zinc-900">Sudoku</h2>
+        </div>
+        <div className='basis-1/6 flex flex-col justify-center hover:scale-110 transition-transform duration-300'>
+          <img src="/images/planets/planet-3.png" alt="Sudoku Planet" className="hover:drop-shadow-2xl | contrast-50 grayscale hue-rotate-90" />
+          <h2 className="text-3xl font-bold text-zinc-900">Coming</h2>
         </div>
       </div>
       <div className="my-8">

a9fbb8e84df671de8bc6ed95bfe5a514965cc0fa
Daniel Fernandez <mdanif9728@gmail.com>
2024-10-03 21:00:47 -0700

    ading audio fx to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,32 @@ import React, { useState } from 'react';
 import WordlePanel from './homePanels/WordlePanel';
 import SpellingPanel from './homePanels/SpellingPanel';
 import SudokuPanel from './homePanels/SudokuPanel';
+import useSound from 'use-sound';
 
 const Home = () => {
+
+  // !-----------------------!-----------------------!
+  //! I want to include sound FX.
+  // !-----------------------!-----------------------!
   const [panelComponent, setPanelComponent] = useState(<WordlePanel />);
-  // !-----------------------!-----------------------!
-  //! I want to include a new planet that set (Coming Sun!).
-  // !-----------------------!-----------------------!
+
+  // AUDIO-FX
+  const [playSound] = useSound('/audio/hover-planet-1.wav');
+  const [selectPlanet] = useSound('/audio/click-on-planet-1.wav')
+  const [noPlanetGame] = useSound('/audio/no-planet.wav')
+
+  function handleHoverPlanet() {
+    playSound();
+  }
+
+  function handleSelect() {
+    selectPlanet();
+  }
+
+  function handleSelectNoPlanet() {
+    noPlanetGame();
+  }
+
   return (
     <div className="text-center mt-8">
       <h1 className="text-6xl font-bold">
@@ -21,25 +41,27 @@ const Home = () => {
       <hr />
       <div className="flex flex-wrap md:space-x-1 space-x-5 justify-center">
         <div className='basis-1/3 flex flex-col justify-center hover:scale-110 transition-transform duration-300'
-          onClick={() => setPanelComponent(<WordlePanel />)}
+          onClick={() => (setPanelComponent(<WordlePanel />), handleSelect())} onMouseEnter={handleHoverPlanet}
         >
           <img src="/images/planets/planet-1.png" alt="Wordle Planet" className="hover:drop-shadow-2xl" />
           <h2 className="text-3xl font-bold text-zinc-900">Wordle</h2>
         </div>
         <div className='basis-1/4 flex flex-col justify-center hover:scale-110 transition-transform duration-300'
-          onClick={() => setPanelComponent(<SpellingPanel />)}
+          onClick={() => (setPanelComponent(<SpellingPanel />), handleSelect())} onMouseEnter={handleHoverPlanet}
         >
           <img src="/images/planets/planet-2.png" alt="Spelling-Bee Planet" className="hover:drop-shadow-2xl" />
           <h2 className="text-3xl font-bold text-zinc-900">Spelling</h2>
         </div>
         <div className='basis-1/5 flex flex-col justify-center hover:scale-110 transition-transform duration-300'
-          onClick={() => setPanelComponent(<SudokuPanel />)}
+          onClick={() => (setPanelComponent(<SudokuPanel />), handleSelect())} onMouseEnter={handleHoverPlanet}
         >
           <img src="/images/planets/planet-3.png" alt="Sudoku Planet" className="hover:drop-shadow-2xl" />
           <h2 className="text-3xl font-bold text-zinc-900">Sudoku</h2>
         </div>
-        <div className='basis-1/6 flex flex-col justify-center hover:scale-110 transition-transform duration-300'>
-          <img src="/images/planets/planet-3.png" alt="Sudoku Planet" className="hover:drop-shadow-2xl | contrast-50 grayscale hue-rotate-90" />
+        <div className='basis-1/6 flex flex-col justify-center hover:scale-110 transition-transform duration-300'
+          onClick={handleSelectNoPlanet}
+        >
+          <img src="/images/planets/planet-3.png" alt="Sudoku Planet" className="hover:drop-shadow-2xl contrast-50 grayscale hue-rotate-90" />
           <h2 className="text-3xl font-bold text-zinc-900">Coming</h2>
         </div>
       </div>
--- src/components/homePanels/WordlePanel.js
+++ src/components/homePanels/WordlePanel.js
@@ -1,55 +1,54 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import useSound from 'use-sound';
 
 export default function WordlePanel() {
+  const [hoverBtn] = useSound('/audio/hover-btn.wav')
+  const [clickBtn] = useSound('/audio/click-btn.wav')
+
+  // AUDIO-FX
+  function handleHoverBTN() {
+    hoverBtn();
+  }
+
+  function handleSelectBTN() {
+    clickBtn();
+  }
+
   return (
-    <div className="border-none md:rounded-xl border-2 border-indigo-600
-        m-auto max-w-2xl p-8 bg-white
-    ">
-      <div className="flex flex-wrap justify-center content-center space-x-8
-        mb-4 bg-zinc-100 rounded-lg
-      ">
+    <div className="border-none md:rounded-xl border-2 border-indigo-600 m-auto max-w-2xl p-8 bg-white">
+      <div className="flex flex-wrap justify-center content-center space-x-8 mb-4 bg-zinc-100 rounded-lg">
         <div className="flex flex-wrap justify-center content-center ">
           <h2 className="text-xl font-bold hover:text-orange-600">
             Word-Le Manual:
           </h2>
         </div>
         <Link to="/wordle"
-          className="
-            tracking-widest m-2 bg-yellow-500 text-white font-bold py-2 px-4 rounded
-            hover:drop-shadow-lg hover:scale-110 hover:bg-green-500
-            transition-transform duration-300
-        ">
+          className=" tracking-widest m-2 bg-yellow-500 text-white font-bold py-2 px-4 rounded
+            hover:drop-shadow-lg hover:scale-110 hover:bg-green-500 transition-transform duration-300"
+          onMouseEnter={handleHoverBTN} onClick={handleSelectBTN}
+        >
           Play
         </Link>
       </div>
-
       <div className="flex flex-col justify-center">
         <h3 className="text-justify m-auto max-w-xl leading-relaxed indent-4">
           Welcome to World-Le,a fun and engaging trivia game where you match
           letters in order to get the correct word. Here’s a quick guide to help you
           understand how to play the game.
         </h3>
-
         <br /> <hr />
-
-        <h2 className="text-lg font-bold bg-zinc-100 rounded-lg my-4 py-2
-          hover:text-orange-600
-        ">
+        <h2 className="text-lg font-bold bg-zinc-100 rounded-lg my-4 py-2 hover:text-orange-600">
           How to Play
         </h2>
         <h3 className="text-justify m-auto max-w-xl leading-relaxed indent-4">
           Word-le is a word puzzle game where the objective is to
           guess a five-letter word within six tries. Here’s how it works:
         </h3>
-
         <br />
-
         <div className="text-justify m-auto max-w-xl leading-relaxed">
           <p className=" my-2 "><strong className="text-orange-600">1- Starting a Game:</strong> You begin by entering any valid five-letter word.</p>
-
           <hr />
-
           <p className=" mt-2 "><strong className="text-orange-600">2- Feedback:</strong> After each guess, the game provides feedback using colored tiles:</p>
           <p className="indent-4">
             <strong className="text-green-600">* Green:</strong> The letter is correct and in the right position.
@@ -60,15 +59,11 @@ export default function WordlePanel() {
           <p className="indent-4">
             <strong className="text-gray-500">* Gray:</strong> The letter is not in the word at all.
           </p>
-
           <hr />
-
           <p className=" my-2 "><strong className="text-orange-600">3- Guessing:</strong> Use the feedback to inform your next guesses.
             For example, if a letter is yellow, you know it’s in the word but needs to be placed differently.
           </p>
-
           <hr />
-
           <p className=" my-2 "><strong className="text-orange-600">4- Winning: </strong>You win by guessing the word correctly within six tries.
             If you don’t guess it, the correct word is revealed at the end.
           </p>
